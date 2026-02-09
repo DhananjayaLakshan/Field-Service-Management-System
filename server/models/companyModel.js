e92af5fbe4ecab9371c9ccfa690fcb5cf84b0fad
DhananjayaLakshan <abey.dhananjayalakshan@gmail.com>
2026-02-09 15:32:09 +0530

    created company model

---
+++ server/models/companyModel.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+
+const companySchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    address: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    addressLink: {
+      type: String,
+      trim: true,
+    },
+    contactPerson: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    contactNumber: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    assignedUser: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      default: null,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+module.exports = mongoose.model("Company", companySchema);
